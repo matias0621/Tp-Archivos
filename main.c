e7ecb41d0143bfb21212f2697a08e20967cc3b15
matias0621 <mattiascorrea@outlook.com>
2024-05-17 12:35:07 -0300

    asd

--- main.c
+++ main.c
@@ -23,6 +23,15 @@ void mostrarAlumnos(strAlumno alumno);
 void mostrarArchivoDeAlumno();
 void cargarAlFinalArchivoDeAlumno();
 void pasarDeUnArchivoAUnaPila(Pila* A);
+int cantidadDeAlumnosPorEdad(int edad);
+void mostrarAlumnosPorMargenDeEdad(int edadMinima, int edadMaxima);
+int cantidadDeAlumnosPorAnio(int anio);
+void mostrarAlumnoDeMayorEdad();
+void buscarAlumnoDeMayorEdad();
+void mostrarDatorDelAlumnoMayor(int edadDelMayor);
+void cantidadDeRegistrosPorSeek();
+void mostrarPorRegistro(int registro);
+void sobreEscribirAlumno(int posicion);
 
 int main()
 {
@@ -43,12 +52,50 @@ int main()
     //cargarAlFinalArchivoDeAlumno();
     //mostrarArchivoDeAlumno();
 
+    /*
     Pila A;
     inicpila(&A);
 
     pasarDeUnArchivoAUnaPila(&A);
     mostrar(&A);
+    */
 
+    /*
+    int edad = cargarNumero();
+
+    int cantidad = cantidadDeAlumnosPorEdad(edad);
+
+    printf("La cantidad de alumnos de %d de edad es de %d", edad, cantidad);
+    */
+
+    /*
+    int edadMinima = cargarNumero();
+    int edadMaxima = cargarNumero();
+
+    mostrarAlumnosPorMargenDeEdad(edadMinima, edadMaxima);
+    */
+
+    /*
+    int anio = cargarNumero();
+
+    int cantidad = cantidadDeAlumnosPorAnio(anio);
+
+    printf("La cantidad de alumnos del anio %d es de %d", anio, cantidad);
+    */
+
+    //buscarAlumnoDeMayorEdad();
+
+    //cantidadDeRegistrosPorSeek();
+
+    /*
+    int registro = cargarNumero();
+    mostrarPorRegistro(registro);
+    */
+    /*
+    int posicion = cargarNumero();
+    sobreEscribirAlumno(posicion);
+    */
+    //mostrarArchivoDeAlumno();
     return 0;
 }
 
@@ -142,8 +189,10 @@ void cargarArchivoDeAlumnos()
 
     buff = fopen(nombreDeArchivoAlumno, "ab");
 
-    if (buff){
-        while(i < 5){
+    if (buff)
+    {
+        while(i < 5)
+        {
             a = cargarAlumnos(a);
             fwrite(&a, sizeof(strAlumno), 1, buff);
             i++;
@@ -153,7 +202,8 @@ void cargarArchivoDeAlumnos()
     fclose(buff);
 }
 
-void mostrarAlumnos(strAlumno alumno){
+void mostrarAlumnos(strAlumno alumno)
+{
     printf("/--------------------------------------------------------------/\n");
     printf("Nombre y Apellido: %s\n", alumno.nombreYApellido);
     printf("Anio del alumno en la carrera: %d\n", alumno.anio);
@@ -187,8 +237,10 @@ void cargarAlFinalArchivoDeAlumno()
 
     buff = fopen(nombreDeArchivoAlumno, "ab");
 
-    if (buff){
-        while(control == 's'){
+    if (buff)
+    {
+        while(control == 's')
+        {
             a = cargarAlumnos(a);
             fwrite(&a, sizeof(strAlumno), 1, buff);
 
@@ -201,7 +253,8 @@ void cargarAlFinalArchivoDeAlumno()
     fclose(buff);
 }
 
-void pasarDeUnArchivoAUnaPila(Pila* A){
+void pasarDeUnArchivoAUnaPila(Pila* A)
+{
     FILE* buff;
     strAlumno aux;
     buff = fopen(nombreDeArchivoAlumno, "rb");
@@ -210,11 +263,208 @@ void pasarDeUnArchivoAUnaPila(Pila* A){
     {
         while((fread(&aux, sizeof(strAlumno), 1, buff)) > 0)
         {
-            if (aux.edad > 18){
+            if (aux.edad > 18)
+            {
                 apilar(A, aux.legajo);
             }
         }
     }
 
+    fclose(buff);
+}
+
+int cantidadDeAlumnosPorEdad(int edad)
+{
+    int cantidadDeAlumnosDeXEdad = 0;
+    FILE* buff;
+    strAlumno aux;
+    buff = fopen(nombreDeArchivoAlumno, "rb");
+
+    if (buff)
+    {
+        while((fread(&aux, sizeof(strAlumno), 1, buff)) > 0)
+        {
+            if (aux.edad > 18)
+            {
+                cantidadDeAlumnosDeXEdad++;
+            }
+        }
+    }
+
+    return cantidadDeAlumnosDeXEdad;
+}
+
+void mostrarAlumnosPorMargenDeEdad(int edadMinima, int edadMaxima)
+{
+    FILE* buff;
+    strAlumno aux;
+    buff = fopen(nombreDeArchivoAlumno, "rb");
+
+    if (buff)
+    {
+        while((fread(&aux, sizeof(strAlumno), 1, buff)) > 0)
+        {
+            if (aux.edad > edadMinima && aux.edad < edadMaxima)
+            {
+                mostrarAlumnos(aux);
+            }
+        }
+    }
+}
+
+int cantidadDeAlumnosPorAnio(int anio)
+{
+    int cantidadDeAlumnosDeXAnio = 0;
+    FILE* buff;
+    strAlumno aux;
+    buff = fopen(nombreDeArchivoAlumno, "rb");
+
+    if (buff)
+    {
+        while((fread(&aux, sizeof(strAlumno), 1, buff)) > 0)
+        {
+            if (aux.anio == anio)
+            {
+                cantidadDeAlumnosDeXAnio++;
+            }
+        }
+    }
+
+    return cantidadDeAlumnosDeXAnio;
+}
+
+void buscarAlumnoDeMayorEdad()
+{
+    int edadDelMayor = 0;
+    FILE* buff;
+    strAlumno aux;
+    buff = fopen(nombreDeArchivoAlumno, "rb");
+
+    if (buff)
+    {
+        while((fread(&aux, sizeof(strAlumno), 1, buff)) > 0)
+        {
+            if (edadDelMayor < aux.edad)
+            {
+                edadDelMayor = aux.edad;
+            }
+        }
+    }
+    fclose(buff);
+    mostrarDatorDelAlumnoMayor(edadDelMayor);
+}
+
+void mostrarDatorDelAlumnoMayor(int edadDelMayor)
+{
+
+    FILE* buff;
+    strAlumno aux;
+    buff = fopen(nombreDeArchivoAlumno, "rb");
+
+    if (buff)
+    {
+        while((fread(&aux, sizeof(strAlumno), 1, buff)) > 0)
+        {
+            if (edadDelMayor == aux.edad)
+            {
+                mostrarAlumnos(aux);
+            }
+        }
+    }
+
+    fclose(buff);
+}
+
+void cantidadDeRegistrosPorSeek(){
+    FILE * buff = fopen(nombreDeArchivoAlumno, "rb");
+
+    int cant = 0;
+
+    if (buff != NULL){
+        fseek(buff, 0, SEEK_END);
+
+        cant = ftell(buff);
+
+        cant = cant/sizeof(strAlumno);
+
+        printf("La cantidad de registros es de: %d", cant);
+    }
+
+    fclose(buff);
+}
+
+void mostrarPorRegistro(int registro){
+    FILE * buff;
+    strAlumno aux;
+    buff = fopen(nombreDeArchivoAlumno, "rb");
+
+    if (buff){
+        rewind(buff);
+        fseek(buff, 0, SEEK_SET);
+        fread(&aux, sizeof(strAlumno)*registro,1,buff);
+        mostrarAlumnos(aux);
+    }
+    fclose(buff);
+}
+
+void sobreEscribirAlumno(int posicion){
+    FILE * buff;
+    strAlumno aux;
+    buff = fopen(nombreDeArchivoAlumno, "r+b");
+
+    if (buff){
+        rewind(buff);
+        fseek(buff, sizeof(strAlumno) * posicion, SEEK_SET);
+        cargarAlumnos(aux);
+        fwrite(&aux, sizeof(strAlumno), 1, buff);
+    }
+    fclose(buff);
+}
+
+void modificarRegistro(int pos){
+    FILE * buff;
+    strAlumno aux;
+    buff = fopen(nombreDeArchivoAlumno, "r+b");
+
+    if (buff){
+        fseek(buff, sizeof(aux)*(pos-1), SEEK_SET);
+        fread(&aux, sizeof(strAlumno),1,buff);
+
+        aux = cargarAlumnos(aux);
+
+        fseek(buff, sizeof(alumno) * (-1), SEEK_CUR);
+        fwrite(&a, sizeof(alumno), 1, buff);
+    }
+    fclose(buff);
+}
+
+void invertirArchivo(){
+    FILE * buff;
+    strAlumno aux, aux1, auxFinal;
+    int inicio = 0, fin = 0, cantidad = 0;
+    buff = fopen(nombreDeArchivoAlumno, "r+b");
+
+    if (buff){
+        fseek(buff, 0, SEEK_END);
+        cantidad = ftell(buff)/ sizeof(strAlumno);
+        fin = cantidad - 1;
+
+        while(inicio < fin){
+            fseek(archi, inicio * sizeof(strAlumno), SEEK_SET);
+            fread(&aux, sizeof(strAlumno), 1, buff);
+
+            fseek(buff, fin*sizeof(strAlumno), SEEK_SET);
+            fread(&aux2, sizeof(strAlumno), 1, buff);
+
+            fseek(archi, inicio * sizeof(strAlumno), SEEK_SET);
+            fwrite(&aux2, sizeof(strAlumno), 1, buff);
+
+            fseek(archi, fin * sizeof(strAlumno), SEEK_SET);
+            fwrite(&aux, sizeof(strAlumno), 1, buff);
+
+            inicio++;
+            fin--;
+        }
+    }
     fclose(buff);
 }
